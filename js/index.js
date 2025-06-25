a38016894683ca6d50ef6791b1d0fc5834b5b0e7
Andres Felipe Diaz <andresf.diazm@unac.edu.co>
2025-06-24 19:22:23 -0500

    New Function

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
+//Sin Try Catch
 async function obtenerUsuarios(){
-    const respuesta = await fetch("https://jsonplaceholder.typicode.com/users");
+    const respuesta = await fetch("https://jsonplaceholder.typicode.com/users")
     const datos = await respuesta.json()
     console.log(datos)
 }
 
-obtenerUsuarios()
+async function obtenerDatos(){
+    try{
+
+        const resp = await fetch("https://jsonplaceholder.typicode.com/posts/1")
+        const data = await resp.json()
+        console.log("Publicacion: ",data)
+
+    }catch(error){
+        console.log("Hubo un error ...", error.message)
+    }
+}
+
+obtenerDatos()
+
